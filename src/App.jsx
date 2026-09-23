e0f2485803f43bc47d3543de3550c9b5397b8256
Johnatas-Mathews <johnatas_mathews@hotmail.com>
2026-09-23 10:02:59 -0300

    Add Estoque page import to App component

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import ContasReceber from './pages/ContasReceber'
 import Relatorios from './pages/Relatorios'
 import Configuracoes from './pages/Configuracoes'
 import Fidelidade from './pages/Fidelidade'
+import Estoque from './pages/Estoque'
 
 // Ícones SVG minimalistas
 const IconDashboard = () => (
